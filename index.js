db012ee49fb182cdc331afa00b971d7a539734f1
David Pinilla <contact@davidpinilla.com>
2020-05-24 00:54:11 +0200

    random change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Web3 from 'web3';
 const { ETH_NODE_URL, ETH_ADDRESS } = process.env;
 const web3 = new Web3(ETH_NODE_URL);
+
 //get ethereum address balance
 const balance = await web3.eth.getBalance(ETH_ADDRESS);
 const balanceEther = web3.utils.fromWei(balance, 'ether')
